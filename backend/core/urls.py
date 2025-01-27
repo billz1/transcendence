2909d357e555fd4417e160d982091c180a874afe
Peniel Yerima <penielyerima@gmail.com>
2025-01-27 12:10:58 +0100

    major update, including pacman game and pong fully functional

--- backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('auth/login/', views.login_view, name='login'),
-    path('auth/callback/', views.callback_view, name='callback'),
-    path('auth/logout/', views.logout_view, name='logout'),
-    path('auth/me/', views.me_view, name='me'),
-]
